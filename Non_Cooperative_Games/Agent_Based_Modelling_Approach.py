0d3ac882682c673585514161f6b21556ab461020
drvinceknight <vincent.knight@gmail.com>
2013-02-28 17:26:58 -0500

    reproduction programmed

--- Non_Cooperative_Games/Agent_Based_Modelling_Approach.py
+++ Non_Cooperative_Games/Agent_Based_Modelling_Approach.py
@@ -1,6 +1,7 @@
 """
 Algorithm from 'Game Theory Evolving'
 """
+import copy
 import random
 
 # Need Individuals
@@ -32,15 +33,36 @@ class Agent():
         self.utility = 0
 
 
-class Collection_of_Agents():
+def kill_one_agent_with_given_utility(agents, utility):
+    number_of_agents = len(agents)
+    k = 0
+    while len(agents) == number_of_agents:
+        if agents[k].utility == utility:
+            del agents[k]
+        k += 1
+
+
+def reproduce_one_agent_with_given_utility(agents, utility, mutation_rate, strategies):
+    number_of_agents = len(agents)
+    k = 0
+    while len(agents) == number_of_agents:
+        if agents[k].utility == utility:
+            agents.append(copy.deepcopy(agents[k]))
+            if random.random() < mutation_rate:
+                agents[-1].strategy = random.choice(strategies)
+        k += 1
+
+
+class ABM():
     """
-    The collection of agents
+    The model
     """
     def __init__(self, number_of_agents, generations, rounds_per_generation, death_rate, mutation_rate, row_matrix, col_matrix):
         self.number_of_agents = number_of_agents
         self.generations = generations
         self.rounds_per_generation = rounds_per_generation
         self.death_rate = death_rate
+        self.number_of_deaths_per_generation = int(number_of_agents * death_rate)
         self.mutation_rate = mutation_rate
         self.row_strategies = range(len(row_matrix))
         self.col_strategies = range(len(col_matrix))
@@ -56,4 +78,19 @@ class Collection_of_Agents():
             self.col_agents[i].utility += self.col_matrix[self.row_agents[i].strategy][self.col_agents[i].strategy]
 
     def reproduce(self):
-        print "More stuff here"
+        d = 0
+        while d < self.number_of_deaths_per_generation:
+            # kill lowest fitness row agent:
+            kill_one_agent_with_given_utility(self.row_agents, min([e.utility for e in self.row_agents]))
+            # reproduce highest fitness row agent:
+            reproduce_one_agent_with_given_utility(self.row_agents, max([e.utility for e in self.row_agents]), self.row_strategies)
+            # kill lowest fitness col agent:
+            kill_one_agent_with_given_utility(self.col_agents, min([e.utility for e in self.col_agents]))
+            # reproduce highest fitness col agent:
+            reproduce_one_agent_with_given_utility(self.col_agents, max([e.utility for e in self.col_agents]), self.col_strategies)
+
+    def simulate(self):
+        for g in range(self.generations):
+            for r in range(self.rounds_per_generation):
+                self.play_tournament()
+            self.reproduce()
