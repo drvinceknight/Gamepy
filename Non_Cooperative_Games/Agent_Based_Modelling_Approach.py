61e15ba907124b6c21c005debfb54f5a20e4e7c0
drvinceknight <vincent.knight@gmail.com>
2013-02-27 22:04:09 -0500

    pseudo code for abm approach coded

--- Non_Cooperative_Games/Agent_Based_Modelling_Approach.py
+++ Non_Cooperative_Games/Agent_Based_Modelling_Approach.py
@@ -7,5 +7,16 @@ Algorithm from 'Game Theory Evolving'
 Define class that has attributes:
 - Strategy
 - Payoff
+"""
 
+# Let me try to code a program that solves matching pennies game...
+
+"""
+- Create population of players,
+- Give them random strategies? (Or pure strategies?)
+- Choose number of rounds per generation, play those rounds keeping everyone's core
+- Once rounds are finished, calculate number of agents that will die due to
+death rate
+- Until those many agents die, kill worst agent and let best agent replicate with mutation probability mu. Mutation here is probability of picking a new strategy.
+- Keep repeating above until number of generations have been created
 """
